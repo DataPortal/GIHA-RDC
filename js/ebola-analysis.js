4fd854794fec0a007e4f0c57ca69726110a38454
UNWOMEN <hypolite.reoutar@unwomen.org>
2026-05-26 23:55:05 +0100

    Update ebola-analysis.js

--- js/ebola-analysis.js
+++ js/ebola-analysis.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     "https://pbxwkrvfzwbnkndjmkui.supabase.co";
 
   const SUPABASE_ANON_KEY =
-    "TA_CLE_ANON_COMPLETE_ICI";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBieHdrcnZmendibmtuZGpta3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyNzI4ODQsImV4cCI6MjA5NDg0ODg4NH0.DnxRQPzJPFuxkK66vS_Epap47mKtwXCpYedaI-87BMw";
 
   const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
